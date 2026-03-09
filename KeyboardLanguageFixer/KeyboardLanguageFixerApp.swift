2797b3ca539113e8829730e6e7d97e3684dfe4ba
David Noy <david4noy@gmail.com>
2026-03-09 13:24:29 +0200

    Initial Commit

---
+++ KeyboardLanguageFixer/KeyboardLanguageFixerApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyboardLanguageFixerApp.swift
+//  KeyboardLanguageFixer
+//
+//  Created by David Noy on 09/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct KeyboardLanguageFixerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
